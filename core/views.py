fdd2ec7c9768d3aa083db259e67f197a81424690
gyonni <giheon424@gmail.com>
2025-07-25 13:44:24 +0900

    main to core

--- core/views.py
+++ core/views.py
@@ -5,4 +5,7 @@ def index(request):
     대문 페이지를 렌더링하는 뷰입니다.
     특별한 로직 없이 main/index.html 템플릿을 보여줍니다.
     """
-    return render(request, 'main/index.html')
+    return render(request, 'core/index.html')
+
+def schedule(request):
+    return render(request, 'core/schedule.html')
