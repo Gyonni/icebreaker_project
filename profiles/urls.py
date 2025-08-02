e7fc009c35a482e864db8570d8fecf3109bfefe6
gyonni <giheon424@gmail.com>
2025-08-02 21:16:19 +0900

    Fix: Resolve NoReverseMatch error for bingo shuffle

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,19 +4,18 @@ from . import views
 app_name = 'profiles'
 
 urlpatterns = [
+    # 빙고 및 사회자 기능 URL
+    path('bingo/', views.bingo_board, name='bingo_board'),
+    path('bingo/shuffle/', views.shuffle_bingo_board, name='shuffle_bingo_board'),
+    path('moderator/random-picker/', views.random_profile_picker, name='random_picker'),
+    path('api/get-random-profile/', views.get_random_profile_data, name='get_random_profile_data'),
+    path('moderator/reset-picks/', views.reset_all_picks, name='reset_all_picks'),
+
+    # 개별 프로필 URL
     re_path(r'^(?P<pk>[a-f0-9-]+)/?$', views.profile_detail, name='profile_detail'),
     re_path(r'^(?P<pk>[a-f0-9-]+)/qr/?$', views.generate_qr, name='generate_qr'),
     re_path(r'^(?P<pk>[a-f0-9-]+)/claim/?$', views.claim_profile, name='claim_profile'),
     re_path(r'^(?P<pk>[a-f0-9-]+)/add/?$', views.add_scanned_person, name='add_scanned_person'),
     re_path(r'^(?P<pk>[a-f0-9-]+)/edit/?$', views.profile_edit, name='profile_edit'),
-    # [새로운 기능] 3T1L 게임 플레이를 처리할 URL 추가
     re_path(r'^(?P<pk>[a-f0-9-]+)/play_3t1l/?$', views.play_3t1l, name='play_3t1l'),
-    # --- [새로운 기능] 빙고 및 사회자 기능 URL 추가 ---
-    path('bingo/', views.bingo_board, name='bingo_board'),
-    # 사회자만 아는 비밀 주소
-    path('moderator/random-picker/', views.random_profile_picker, name='random_picker'),
-    # 사회자 페이지에서 다음 사람을 뽑기 위한 API 주소
-    path('api/get-random-profile/', views.get_random_profile_data, name='get_random_profile_data'),
-    path('moderator/reset-picks/', views.reset_all_picks, name='reset_all_picks'),
 ]
-
