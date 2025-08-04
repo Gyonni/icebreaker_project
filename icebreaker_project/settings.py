cce78a9263b8f112bdff1c4999cbff4946068cdd
gyonni <giheon424@gmail.com>
2025-08-04 23:24:24 +0900

    Feat: Add prayerboard app and PrayerRequest model

--- icebreaker_project/settings.py
+++ icebreaker_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'profiles',
     'core',
     'config',
+    'prayerboard',
 ]
 
 MIDDLEWARE = [
---
+++ prayerboard/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class PrayerRequest(models.Model):
+    content = models.TextField("기도제목 내용")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"기도제목 ({self.created_at.strftime('%Y-%m-%d %H:%M')})"
+
+    class Meta:
+        verbose_name = "익명 기도제목"
+        verbose_name_plural = "익명 기도제목 목록"
+        ordering = ['-created_at'] # 항상 최신순으로 정렬
